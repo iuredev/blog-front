af2bfa75d71aa2d0a38813ab63168ee45ff60db7
Iure Silva <iure.sgz@gmail.com>
2025-03-07 11:28:49 -0300

    chore: remove console.log from vite.config

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react-swc";
 // https://vite.dev/config/
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), "");
-  console.log(env);
+
   return {
     server: {
       port: parseInt(env.VITE_PORT || "5174"),
