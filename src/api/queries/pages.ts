683312aa87f3966845cbf5297b10557a9be6221f
Iure Silva <iure.sgz@gmail.com>
2025-03-07 13:25:17 -0300

    chore: add proxy /api to vite.config

--- src/api/queries/pages.ts
+++ src/api/queries/pages.ts
@@ -5,7 +5,9 @@ const VITE_API_KEY = import.meta.env.VITE_API_KEY;
 const VITE_API_URL = import.meta.env.VITE_API_URL;
 
 export const getPage = async (page: PageType) => {
-  const response = await axios.get(`${VITE_API_URL}/${page}?populate=*`, {
+  console.log("VITE_API_URL", VITE_API_URL);
+
+  const response = await axios.get(`${VITE_API_URL}/api/${page}?populate=*`, {
     headers: {
       Authorization: `Bearer ${VITE_API_KEY}`,
     },
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,6 @@
 import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react-swc";
 
-// https://vite.dev/config/
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), "");
 
@@ -20,6 +19,16 @@ export default defineConfig(({ mode }) => {
         "iure.site",
         "dev.iure.site",
       ],
+
+      proxy: {
+        "/api": {
+          target: `${env.VITE_API_URL}/api`,
+        },
+
+        "/admin": {
+          target: `${env.VITE_API_URL}/admin`,
+        },
+      },
     },
     plugins: [react()],
   };
