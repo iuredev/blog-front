daff2afff7caeea45172bd9fab6939b1b89166e6
Iure Silva <iure.sgz@gmail.com>
2025-03-07 13:25:17 -0300

    chore: add proxy /api to vite.config

--- src/api/queries/posts.ts
+++ src/api/queries/posts.ts
@@ -5,8 +5,11 @@ const VITE_API_KEY = import.meta.env.VITE_API_KEY;
 const VITE_API_URL = import.meta.env.VITE_API_URL;
 
 export const getPosts = async (pageSize: number, page?: number) => {
+  console.log("url", VITE_API_URL);
+  console.log("key", VITE_API_KEY);
+
   const response = await axios.get(
-    `${VITE_API_URL}/articles?pagination[page]=${
+    `/api/articles?pagination[page]=${
       page || 1
     }&pagination[pageSize]=${pageSize}&populate=*&sort=createdAt:desc`,
     {
@@ -21,7 +24,7 @@ export const getPosts = async (pageSize: number, page?: number) => {
 
 export const getPostBySlug = async (slug: string) => {
   const response = await axios.get(
-    `${VITE_API_URL}/articles?filters[slug][$eq]=${slug}&populate=*`,
+    `/api/articles?filters[slug][$eq]=${slug}&populate=*`,
     {
       headers: {
         Authorization: `Bearer ${VITE_API_KEY}`,
