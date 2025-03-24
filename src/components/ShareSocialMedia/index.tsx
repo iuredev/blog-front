1a66597470f7c7c5612932320ce1ac4f626e9363
Iure Silva <iure.sgz@gmail.com>
2025-03-24 17:52:09 -0300

    fix: og-contents from helmet hook

---
+++ src/components/ShareSocialMedia/index.tsx
@@ -0,0 +1,20 @@
+import { FaLinkedin } from "react-icons/fa6";
+
+interface ShareSocialMediaProps {
+  url: string;
+}
+export default function ShareSocialMedia({ url }: ShareSocialMediaProps) {
+
+  const encodedUrl = encodeURIComponent(url);
+
+  const linkedinShareUrl = `https://www.linkedin.com/sharing/share-offsite/?url=${encodedUrl}`;
+
+  return (
+    <div className="flex items-center gap-1 leading-none">
+      <p className="text-[1.0rem] text-gray-400">Share on:</p>
+      <a href={linkedinShareUrl} className="text-xm text-gray-400 hover:text-gray-600" aria-label="Share on LinkedIn" title="Share on LinkedIn" target="_blank" rel="noopener noreferrer">
+        <FaLinkedin className="h-4 w-4 text-gray-300" />
+      </a>
+    </div>
+  );
+}
--- src/pages/Blog/index.tsx
+++ src/pages/Blog/index.tsx
@@ -1,17 +1,17 @@
-import {  useGetPostsPaginated } from "../../api/hooks";
+import { useGetPostsPaginated } from "../../api/hooks";
 import { useState } from "react";
 import { Error, Loading, PostLink } from "../../components";
 import Pagination from "../../components/Pagination";
-import useHelmet from "../../hooks";
+import { useHelmet } from "../../hooks";
 import EmptyPosts from "../EmptyPosts";
 
 export default function Blog() {
   const [currentPage, setCurrentPage] = useState(1);
-  const {posts = [], pagination, isLoading, isError } = useGetPostsPaginated(10, currentPage);
+  const { posts = [], pagination, isLoading, isError } = useGetPostsPaginated(10, currentPage);
 
   useHelmet("Blog", "Blog - Iure.dev");
 
- const render = () => {
+  const render = () => {
     if (isLoading) {
       return <Loading />;
     }
@@ -27,24 +27,24 @@ export default function Blog() {
 
     return (
       posts.map((post) => (
-          <PostLink key={post.id} post={post} preview={false} />
-          ))
-        )
+        <PostLink key={post.id} post={post} preview={false} />
+      ))
+    )
 
   }
-  
+
 
   return (
     <div>
-      <div > 
+      <div >
         <h1 className="text-4xl font-bold ">Blog</h1>
         <p className="text-gray-400">This is where I write about things that interest me. Enjoy reading it! 🙂</p>
       </div>
-     
+
 
       <div className="mt-8">
-        {render()}        
-        <Pagination totalPages={pagination && pagination?.pageCount || 1} currentPage={currentPage} setCurrentPage={setCurrentPage}/>
+        {render()}
+        <Pagination totalPages={pagination && pagination?.pageCount || 1} currentPage={currentPage} setCurrentPage={setCurrentPage} />
       </div>
     </div>
   );
